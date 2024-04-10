e8036faaf91849776a9b0152d7560a73cb9b8473
Fursanov <serezha.fursanov@mail.ru>
2024-04-10 14:52:36 +0300

    fix warnings and add environment variables

--- src/main/java/com/example/onlineMarket/entity/Store.java
+++ src/main/java/com/example/onlineMarket/entity/Store.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import jakarta.persistence.*;
 
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Getter
--- src/main/java/com/example/onlineMarket/entity/User.java
+++ src/main/java/com/example/onlineMarket/entity/User.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.util.Set;
 
 @Entity
